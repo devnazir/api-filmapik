74bbd8d8a42c7c2af4ecabfadb4b333e7c60255d
devnazir <nazirbachrululum@gmail.com>
2021-02-19 16:39:44 +0700

    added params

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -11,7 +11,7 @@ async function scrapping(res, url) {
     }
 }
 
-async function getJSON(req, res, url, numPage) {
+async function getJSON(req, res, url, numPage, gdrive) {
     try {
         const html = await scrapping(res, url)
         const { window } = new JSDOM(html)
@@ -20,59 +20,121 @@ async function getJSON(req, res, url, numPage) {
         const movies = Array.from(window.document.querySelectorAll('.movies-list .ml-item'))
         await Promise.all(movies.map(async movie => {
             const officialWeb = movie.querySelector('.ml-mask').getAttribute('href')
-            const quality = movie.querySelector('.mli-quality').textContent
+            const quality = movie.querySelector('.mli-quality')?.textContent
+            const episode = movie.querySelector('.mli-eps')?.textContent
             const rating = movie.querySelector('.mli-rating').textContent
             const thumbnail = movie.querySelector('img').getAttribute('data-original')
             const title = movie.querySelector('.mli-info h2').textContent
 
             result.push({
                 title,
-                thumbnail,
+                thumbnailPotrait: thumbnail,
                 rating,
                 quality,
+                episode,
+                detail: await detailMovie(officialWeb),
                 officialWeb,
-                video: await getIframe(req, officialWeb)
+                video: await getIframe(req, res, officialWeb, gdrive)
             })
         }))
 
         result.forEach(movie => {
+            for(const data in movie.detail) {
+                if(movie.detail[data] === null || movie.detail[data] === 0) {
+                    delete movie.detail[data]
+                }
+            }
             delete movie.officialWeb
         })
 
         res.json({ result, page: numPage })
+
     } catch (err) {
         console.log(err)
     }
 }
 
-async function getIframe(req, officialWeb) {
+async function detailMovie(officialWeb) {
+
+    if(officialWeb.includes('tvshows')) {
+        const urlTemp = new URL(officialWeb).toString().split('/tvshows').join("")
+        const url = new URL(urlTemp)
+
+        officialWeb = `${url.origin}/episodes${url.pathname}-1x1/play`
+    }
+
+    const html = await axios(officialWeb)
+    const { window } = new JSDOM(html.data)
+
+    const views = window.document.querySelector('.mvic-desc .mvic-info .mvici-left')?.children.item(0)?.textContent.split(" ")?.slice(1).join(" ")
+    const genre = window.document.querySelector('.mvic-desc .mvic-info .mvici-left')?.children.item(1)?.textContent.split(" ")?.slice(1).join(" ")
+    const director = window.document.querySelector('.mvic-desc .mvic-info .mvici-left')?.children.item(2)?.querySelector('span')?.textContent
+    const actors = window.document.querySelector('.mvic-desc .mvic-info .mvici-left')?.children.item(3)?.querySelector('span')?.textContent
+    const country = window.document.querySelector('.mvic-desc .mvic-info .mvici-left')?.children.item(4)?.children.item(1)?.textContent
+
+    const duration = window.document.querySelector('.mvic-desc .mvic-info .mvici-right')?.children.item(0)?.querySelector('span')?.textContent
+    const release = window.document.querySelector('.mvic-desc .mvic-info .mvici-right')?.children.item(2)?.textContent.split(" ")?.slice(1).join(" ")
+    const description = window.document.querySelector('.mvic-desc .desc')?.textContent
+    const thumbnailLandscape = window.document.querySelector('#mv-info a')?.getAttribute('style')?.split(" ")?.slice(1).join("").split("url").slice(1).join("").split("(").slice(1).join("").split(")").join("")
+    const trailer = window.document.querySelector('.mvic-desc')?.children.item(1)?.children.item(0)?.getAttribute('href')
+    const eps = window.document.querySelectorAll('.episodios a')?.length
+
+    return {
+        trailer: trailer ?? null,
+        views: views ?? null,
+        genre: genre ?? null,
+        director: director ?? null,
+        actors: actors ?? null,
+        country: country ?? null,
+        duration: duration ?? null,
+        release: release ?? null,
+        thumbnailLandscape: thumbnailLandscape ?? null,
+        description: description ?? null,
+        totalEpisodes: eps ?? null
+    }
+}
+
+async function getIframe(req, res, officialWeb, gdrive) {
     try {
         let playVideo = `${officialWeb}/play`
-        // if (req.url === "tvshows") {
-        //     playVideo = `${officialWeb}`
-        // }
-        const response = await axios(playVideo)
-        const { window } = new JSDOM(response.data)
-        const iframe = window.document.querySelector('.iframe #myFrame').getAttribute('src')
-        return await getVideoLink(iframe)
+        let response = await axios(playVideo)
+        let { window } = new JSDOM(response.data)
+        let iframe = window.document.querySelector('.iframe #myFrame')?.getAttribute('src')
+
+        if (iframe === undefined || iframe === null) {
+            const urlTemp = new URL(officialWeb).toString().split('/tvshows').join("")
+            const url = new URL(urlTemp)
+
+            playVideo = `${url.origin}/episodes${url.pathname}-1x1/play`
+            response = await axios(playVideo)
+            const { window } = new JSDOM(response.data)
+            iframe = window.document.querySelector('.iframe #myFrame').getAttribute('src')
+        }
+
+        if (gdrive == "true") {
+            return await getVideoLink(res, iframe)
+        }
+
+        return iframe
+
     } catch (err) {
         console.log(err)
     }
 }
 
-async function getVideoLink(iframe) {
+async function getVideoLink(res, iframe) {
     try {
         const response = await axios(iframe)
         const { window } = new JSDOM(response.data)
         const linkVideo = window.document.querySelector("iframe").src
         return linkVideo
     } catch (err) {
-        console.log(err)
+        return res.json({ error: err.response.statusText })
     }
 }
 
-function getMovies(req, res, url, numPage) {
-    getJSON(req, res, url, numPage)
+function getMovies(req, res, url, numPage, gdrive) {
+    getJSON(req, res, url, numPage, gdrive)
 }
 
 module.exports = getMovies
--- server.js
+++ server.js
@@ -2,30 +2,90 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const getMovies = require('./scraper/scraper')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const axios = require('axios')
+
+const domain = 'http://103.194.171.18'
 
 app.use(cors({ origin: '*' }))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.json({
         docs: 'Please read documentation at https://github.com/devnazir/api-filmapik'
     })
 })
 
-app.get('/latest', function(req, res) {
-    let numPage = req.query.page ?? "1"
-    const url = `http://103.194.171.18/latest/page/${numPage}`
-    getMovies(req, res, url, numPage)
+function checkNumPage(req) {
+    return req.query.page ?? "1"
+}
+
+function checkGdrive(req) {
+    return req.query.gdrive ?? false
+}
+
+function checkSearch(req) {
+    return req.query.search.toLowerCase()
+}
+
+async function scrapHomePage() {
+    const html = await axios(`${domain}`)
+    const response = html.data
+    const { window } = new JSDOM(response)
+
+    return window
+}
+
+
+app.get('/search', function(req, res) {
+    const q = req.query.q
+    const url = `${domain}/page/${checkNumPage(req)}?s=${q}`
+    getMovies(req, res, url, checkNumPage(req), checkGdrive(req))
 })
 
-// app.get('/tvshows', function(req, res) {
-//     let numPage = req.query.page ?? "1"
-//     const url = `http://103.194.171.18/tvshows/page/${numPage}`
-//     getMovies(req, res, url, numPage)
-// })
+app.get('/latest', function (req, res) {
+    const url = `${domain}/latest/page/${checkNumPage(req)}`
+    getMovies(req, res, url, checkNumPage(req), checkGdrive(req))
+})
 
-app.get('*', function(req, res) {
-    res.send({error: "Error"})
+app.get('/country', async function (req, res) {
+    const country = []
+
+    const items = (await scrapHomePage()).document.querySelectorAll('#menu li#menu-item-13566 .menu-item')
+    items.forEach(item => {
+        country.push(item.textContent.toLowerCase())
+    })
+
+    if (country.includes(checkSearch(req))) {
+        const url = `${domain}/negara/${checkSearch(req)}/page/${checkNumPage(req)}`
+        getMovies(req, res, url, checkNumPage(req), checkGdrive(req))
+        return
+    }
+
+    res.json({ error: `Movies in ${checkSearch(req)} not found` })
+})
+
+app.get('/category', async function(req, res) {
+    const category = []
+
+    const items = (await scrapHomePage()).document.querySelectorAll('#menu li.menu-item .menu-item-object-category')
+    items.forEach(item => {
+        category.push(item.textContent.toLowerCase())
+    })
+
+    category.push('box-office', 'boxoffice')
+
+    if (category.includes(checkSearch(req))) {
+        const url = `${domain}/category/${checkSearch(req)}/page/${checkNumPage(req)}`
+        getMovies(req, res, url, checkNumPage(req), checkGdrive(req))
+        return
+    }
+
+    res.json({ error: `Category ${checkSearch(req)} not found` })
+})
+
+app.get('*', function (req, res) {
+    res.send({ error: "Error" })
 })
 
 app.listen(process.env.PORT || 8000, () => console.log("Server work at localhost:8000"))
-
